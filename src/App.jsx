2527a49e1cc55d22599fd9d2e4b2e20728ea9d9a
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-11-11 11:52:44 +0100

    add map

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import { useState, useEffect } from 'react'
-
 import './App.css'
 
 function App() {
 
+const list = [
+  { title: 'Inception', genre: 'Fantascienza' },
+   { title: 'Il Padrino', genre: 'Thriller' },
+   { title: 'Titanic', genre: 'Romantico' },
+   { title: 'Batman', genre: 'Azione' },
+   { title: 'Interstellar', genre: 'Fantascienza' },
+   { title: 'Pulp Fiction', genre: 'Thriller' },
+ ]
+ 
 
   return (
     <>
+      <h1>Scegli il film che vuoi cercare 🎥</h1>
+
+      <div className='container'>
+      {
+        list.map(lista =>
+          <div className='card'>
+            <h3>{lista.title}🎬</h3>
+          </div>
+        )
+      }
+      </div>
       
     </>
   )
